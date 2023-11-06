a2e7128f6f3a35eb82e97212e57206715b98827f
Ashiq Cherian <67139570+ninjahurricane007@users.noreply.github.com>
2023-11-06 19:00:05 +0530

    Added files via upload

---
+++ script.js
@@ -0,0 +1,124 @@
+let hasPlayerWon = false;
+
+
+//get active player number
+function getActivePlayer() {
+    const player0 = document.getElementsByClassName('player player--0')[0];
+    const player1 = document.getElementsByClassName('player player--1')[0];
+ 
+    if (player0.classList.contains('player--active')) {
+        return 0;
+    } else if (player1.classList.contains('player--active')) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+
+//new game button function
+function reset()
+{
+    const popup = document.getElementById('popup');
+    if (popup) {
+        popup.remove();
+        hasPlayerWon = false;
+    }
+    document.getElementById('current--0').textContent=0;
+    document.getElementById('current--1').textContent=0;
+    document.getElementById('score--0').textContent=0;
+    document.getElementById('score--1').textContent=0;
+    const diceImage = document.querySelector(".dice");
+    const imageName = ``;
+    diceImage.src = imageName;
+
+    current=0;
+    currentVal=0;
+}
+
+let current = 0;
+
+
+//random number generator
+function rollDice() {
+    if(hasPlayerWon){
+        return;
+    }
+    const playerNumber = getActivePlayer();
+    const random = Math.floor(Math.random() * 6) + 1;
+    const diceImage = document.querySelector(".dice");
+    const imageName = `/img/dice-${random}.png`;
+    diceImage.src = imageName;
+    if (random !== 1) {
+        current = current + random;
+    } else {
+        current = 0;
+        document.getElementById(`current--${playerNumber}`).textContent = current;
+        switchPlayer();
+    }
+    console.log("random = " + random);
+    console.log("current = " + current);
+    document.getElementById(`current--${playerNumber}`).textContent = current;
+    return current;
+}
+
+
+//switch between players
+  function  switchPlayer() {
+  const player1 = document.getElementsByClassName('player player--0')[0];
+  const player2 = document.getElementsByClassName('player player--1')[0];
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+
+  player1.classList.toggle('player--active');
+  player2.classList.toggle('player--active');
+};
+
+
+//hold function
+function hold() {
+    if(hasPlayerWon){
+        return;
+    }
+    let current0 = parseFloat(document.querySelector('#current--0').textContent);
+    let current1 = parseFloat(document.querySelector('#current--1').textContent);
+    let score0 = parseFloat(document.querySelector('#score--0').textContent);
+    let score1 = parseFloat(document.querySelector('#score--1').textContent);
+    document.getElementById('score--0').textContent = score0 + current0;
+    document.getElementById('score--1').textContent = score1 + current1;
+
+    document.getElementById('current--0').textContent = 0;
+    document.getElementById('current--1').textContent = 0;
+
+    if(score0 + current0 >=50){
+        congratulatePlayer(0)
+    } else if(score1 + current1 >=50){
+        congratulatePlayer(1)
+    }
+    switchPlayer();
+
+    current=0;
+    currentVal=0;
+  }
+
+
+//winner displaying
+function congratulatePlayer(activePlayer) {
+    const congratsMessage = `Congratulations, Player${activePlayer + 1}!`;
+    const popup = document.createElement('div');
+    popup.id = 'popup';
+    popup.className = 'popup';
+    popup.textContent = congratsMessage;
+    popup.style.top = '47%';
+    popup.style.backgroundColor = 'rgba(255, 255, 255, 0.568)';
+    popup.style.position = 'absolute';
+    popup.style.padding = '15px';
+    popup.style.fontSize = '40px';
+    popup.style.border = '3px solid #c7365f';
+    popup.style.borderRadius = '100px'
+    popup.style.textAlign = 'center';
+    popup.style.zIndex = '99';
+    popup.style.animation = 'sparkle 0.8s infinite';
+    document.body.appendChild(popup);
+    hasPlayerWon = true;
+}
